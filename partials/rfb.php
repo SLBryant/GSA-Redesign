17be26ac3a384e62b3089b051fe2db51714c10fd
Adam Pflantzer <apflantzer@gmail.com>
2014-09-29 16:44:28 -0400

    RFB image size update

--- partials/rfb.php
+++ partials/rfb.php
@@ -1,26 +1,26 @@
 <div class="rotating-feature-block">
     <div class="row">
-        <figure class="col-xs-5">
-            <img class="img-responsive" src="http://placehold.it/230x230">
+        <figure class="col-xs-6">
+            <img class="img-responsive" src="http://placehold.it/430x230">
         </figure>
         <article class="rfb-content col-xs-6">
             <header>
                 <h4>Free Webinar</h4>
                 <p>Register to learn about the flexibility of the GSA Schedules program. These free webinars are offered monthly.</p>
             </header>
-            <a href="#">Find out More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <a href="#">Find out More <span class="icon-arrow-right"></span></a>
         </article>
     </div>
     <div class="row">
-        <figure class="col-xs-5">
-            <img class="img-responsive" src="http://placehold.it/230x230">
+        <figure class="col-xs-6">
+            <img class="img-responsive" src="http://placehold.it/430x230">
         </figure>
         <article class="rfb-content col-xs-6">
             <header>
                 <h4>Free Webinar</h4>
                 <p>Register to learn about the flexibility of the GSA Schedules program. These free webinars are offered monthly.</p>
             </header>
-            <a href="#">Find out More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <a href="#">Find out More <span class="icon-arrow-right"></span></a>
         </article>
     </div>
 </div>
