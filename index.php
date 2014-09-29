1fac511496fbc501bd753b7bc67f1083d9a7869d
Adam Pflantzer <apflantzer@gmail.com>
2014-09-29 12:57:57 -0400

    relative urls

--- index.php
+++ index.php
@@ -12,72 +12,72 @@
         <tbody>
             <tr>
                 <td>Home</td>
-                <td><a href="/home.php">home.php</a></td>
+                <td><a href="home.php">home.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/home1.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/home1.html</a></td>
             </tr>
             <tr>
                 <td>Travel</td>
-                <td><a href="/travel.php">travel.php</a></td>
+                <td><a href="travel.php">travel.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/tabtravel.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/tabtravel.html</a></td>
             </tr>
             <tr>
                 <td>Real Estate</td>
-                <td><a href="/real-estate.php">real-estate.php</a></td>
+                <td><a href="real-estate.php">real-estate.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/tabre1.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/tabre1.html</a></td>
             </tr>
             <tr>
                 <td>Shoppers</td>
-                <td><a href="/shop.php">shop.php</a></td>
+                <td><a href="shop.php">shop.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/tabshopper.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/tabshopper.html</a></td>
             </tr>
             <tr>
                 <td>Technology</td>
-                <td><a href="/technology.php">technology.php</a></td>
+                <td><a href="technology.php">technology.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/tabtech.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/tabtech.html</a></td>
             </tr>
             <tr>
                 <td>Strategically Sustainable</td>
-                <td><a href="/sustainability.php">sustainability.php</a></td>
+                <td><a href="sustainability.php">sustainability.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/feature3.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/feature3.html</a></td>
             </tr>
             <tr>
                 <td>Telework</td>
-                <td><a href="/telework.php">telework.php</a></td>
+                <td><a href="telework.php">telework.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/feature2.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/feature2.html</a></td>
             </tr>
             <tr>
                 <td>Transportation Programs</td>
-                <td><a href="/transportation.php">transportation.php</a></td>
+                <td><a href="transportation.php">transportation.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/feature1.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/feature1.html</a></td>
             </tr>
             <tr>
                 <td>Two Columns (no sidebar)</td>
-                <td><a href=/two-columns.php">/two-columns.php</a></td>
+                <td><a href=two-columns.php">/two-columns.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/microsite2.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/microsite2.html</a></td>
             </tr>
             <tr>
                 <td>Two Columns (no nav)</td>
-                <td><a href="/two-columns-no-nav.php">two-columns-no-nav.php</a></td>
+                <td><a href="two-columns-no-nav.php">two-columns-no-nav.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/microsite4.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/microsite4.html</a></td>
             </tr>
             <tr>
                 <td>Two Columns (no nav per-diem)</td>
-                <td><a href="/two-columns-no-nav-perdiem.php">two-columns-no-nav-perdiem.php</a></td>
+                <td><a href="two-columns-no-nav-perdiem.php">two-columns-no-nav-perdiem.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/perdiem1.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/perdiem1.html</a></td>
             </tr>
             <tr>
                 <td>Three Columns (schedules)</td>
-                <td><a href="/three-columns.php">three-columns.php</a></td>
+                <td><a href="three-columns.php">three-columns.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/microsite1.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/microsite1.html</a></td>
             </tr>
             <tr>
                 <td>Region 8</td>
-                <td><a href="/region.php">region.php</a></td>
+                <td><a href="region.php">region.php</a></td>
                 <td><a href="http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/region1.html">http://workarea.ogilvyaction.com/gsa/2014_redesign_templates/region1.html</a></td>
             </tr>
             <tr>
                 <td>Tables</td>
-                <td><a href="/tables.php">tables.php</a></td>
+                <td><a href="tables.php">tables.php</a></td>
             </tr>
         </tbody>
     </table>
