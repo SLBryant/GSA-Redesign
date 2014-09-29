dccc1c9fd99df8382f12feb2b91d0ba319aa90d9
Adam Pflantzer <apflantzer@gmail.com>
2014-09-29 15:03:30 -0400

    Inner Page Navigation responsive updates

--- partials/search-in-nav.php
+++ partials/search-in-nav.php
@@ -2,6 +2,6 @@
 <form class="hidden" id="main-nav-search-field" role="search">
     <div class="search-input form-group">
         <input type="text" class="form-control" placeholder="Search">
-        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-chevron-right"></span></button>
+        <button type="submit" class="btn btn-default"><span class="icon-arrow-right"></span></button>
     </div>
 </form>
