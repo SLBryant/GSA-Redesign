fa2badefbf84f3a19459dd75537c2ee572efe877
Adam Pflantzer <apflantzer@gmail.com>
2014-10-06 14:20:36 -0400

    RFB responsiveness updates

--- partials/rfb.php
+++ partials/rfb.php
@@ -1,9 +1,9 @@
 <div class="rotating-feature-block">
     <div class="row">
-        <figure class="col-xs-6">
+        <figure class="col-xs-6 col-md-6 col-sm-3">
             <img class="img-responsive" src="http://placehold.it/430x230">
         </figure>
-        <article class="rfb-content col-xs-6">
+        <article class="rfb-content col-xs-6 col-md-6 col-sm-9">
             <header>
                 <h4>Free Webinar</h4>
                 <p>Register to learn about the flexibility of the GSA Schedules program. These free webinars are offered monthly.</p>
@@ -12,10 +12,10 @@
         </article>
     </div>
     <div class="row">
-        <figure class="col-xs-6">
+        <figure class="col-xs-6 col-md-6 col-sm-3">
             <img class="img-responsive" src="http://placehold.it/430x230">
         </figure>
-        <article class="rfb-content col-xs-6">
+        <article class="rfb-content col-xs-6 col-md-6 col-sm-9">
             <header>
                 <h4>Free Webinar</h4>
                 <p>Register to learn about the flexibility of the GSA Schedules program. These free webinars are offered monthly.</p>
--- partials/sidebar-accordion-nav.php
+++ partials/sidebar-accordion-nav.php
@@ -1,7 +1,7 @@
 <aside id="sidebar-nav-wrapper" class="col-sm-3 hidden-xs" data-spy="affix" data-offset-bottom="300">
     <div role="tablist">
         <ul class="sidebar-accordion-nav sm sm-vertical">
-            <li role="tab"><a href="#">Top level Link</a>
+            <li role="tab"><a href="#">Top level Link (expands)</a>
                 <ul>
                     <li><a href="#">Level Two (Expands)</a>
                         <ul>
@@ -11,7 +11,7 @@
                     </li>
                 </ul>
             </li>
-            <li role="tab"><a href="#">Top level Link</a>
+            <li role="tab"><a href="#">Top level Link (expands)</a>
                 <ul>
                     <li><a href="#">Level Two Link</a></li>
                     <li><a href="#">Level Two (expands)</a>
@@ -21,8 +21,53 @@
                         </ul>
                     </li>
                     <li><a href="#">Level Two Link</a></li>
+                    <li><a href="#">Level Two (expands)</a>
+                        <ul>
+                            <li><a href="#">Level Three Link</a></li>
+                            <li><a href="#">Level Three Link</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Level Two (expands)</a>
+                        <ul>
+                            <li><a href="#">Level Three Link</a></li>
+                            <li><a href="#">Level Three Link</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Level Two Link</a></li>
+                    <li><a href="#">Level Two Link</a></li>
                 </ul>
             </li>
+            <li role="tab"><a href="#">Top level Link (expands)</a>
+                <ul>
+                    <li><a href="#">Level Two Link</a></li>
+                    <li><a href="#">Level Two (expands)</a>
+                        <ul>
+                            <li><a href="#">Level Three Link</a></li>
+                            <li><a href="#">Level Three Link</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Level Two Link</a></li>
+                    <li><a href="#">Level Two (expands)</a>
+                        <ul>
+                            <li><a href="#">Level Three Link</a></li>
+                            <li><a href="#">Level Three Link</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Level Two (expands)</a>
+                        <ul>
+                            <li><a href="#">Level Three Link</a></li>
+                            <li><a href="#">Level Three Link</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Level Two Link</a></li>
+                    <li><a href="#">Level Two Link</a></li>
+                </ul>
+            </li>
+            <li role="tab"><a href="#">Top level Link</a></li>
+            <li role="tab"><a href="#">Top level Link</a></li>
+            <li role="tab"><a href="#">Top level Link</a></li>
+            <li role="tab"><a href="#">Top level Link</a></li>
+            <li role="tab"><a href="#">Top level Link</a></li>
         </ul>
     </div>
 </aside>
