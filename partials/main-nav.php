bab8f8f9c00b1272363259da0448e1eae4e0d53c
Adam Pflantzer <apflantzer@gmail.com>
2014-10-09 16:05:54 -0400

    Partial Upload of Navigation Items
    More to come shortly

---
+++ partials/main-nav.php
@@ -0,0 +1,295 @@
+<ul class="nav navbar-nav" id="main-nav">
+<!-- top 1 -->
+<li><a href="travel.php">Travel</a>
+    <ul class="dropdown-menu">
+        <li><a href="#">Per Diem Info</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Lookup Rates</a></li>
+                <li><a href="#">Meals &amp; Incidental Expenses (M&amp;IE)</a></li>
+                <li><a href="#">Mileage Reimbursement Rates (POV)</a></li>
+                <li><a href="#">State Tax Exemption Forms</a></li>
+                <li><a href="#">Frequently Asked Questions</a></li>
+                <li><a href="#">More Per Diem Info</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Travel Planning &amp; Booking</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Airfare rates (City Pairs)</a></li>
+                <li><a href="#">Hotels &amp; Lodging</a></li>
+                <li><a href="#">Rental Car Rates (Source: DoD)</a></li>
+                <li><a href="#">Travel Booking Service (ETS)</a></li>
+                <li><a href="#">Travel Charge Card (SmartPay)</a></li>
+                <li><a href="#">Travel Training</a></li>
+                <li><a href="#">More Travel Planning Info</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Transportation &amp; Relocation Services</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Employee Relocation</a></li>
+                <li><a href="#">Electronic Bill of Lading (BOL)</a></li>
+                <li><a href="#">Freight Management</a></li>
+                <li><a href="#">Household Goods Transportation</a></li>
+                <li><a href="#">TMSS (Household Goods BOL)</a></li>
+                <li><a href="#">TransPort Integrator (Freight BOL)</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Vehicles &amp; Fleet Service</a></li>
+        <li><a href="#">Business Opportunities</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Freight Request for Offer (RFO) and Standard Tender of Service (STO)</a></li>
+                <li><a href="#">Hotel Participation in FedRooms</a></li>
+                <li><a href="#">Household Goods Request for Offer (RFO) and Standard Tender of Service (STO)</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Policy</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Federal Fleet Mandates</a></li>
+                <li><a href="#">Federal Travel Regulation (FTR)</a></li>
+                <li><a href="#">FTR: Temp Duty (TDY) Allowances / Travel FAQs</a></li>
+                <li><a href="#">FTR: Relocation Allowances FAQs</a></li>
+                <li><a href="#">Travel and Relocation Policy</a></li>
+                <li><a href="#">Fly America Act</a></li>
+                <li><a href="#">Travel Management</a></li>
+                <li><a href="#">Vehicle Management Policy</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Schedules</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Automotive Superstore (23 V)</a></li>
+                <li><a href="#">Autochoice (23 V)</a></li>
+                <li><a href="#">Leasing of Automobiles and Light Trucks (751)</a></li>
+                <li><a href="#">Transportation, Delivery &amp; Relocation Services (48)</a></li>
+                <li><a href="#">Travel Service Solutions (599)</a></li>
+            </ul>
+        </li>
+        <figure class="menu-image">
+            <img src="images/menu-travel.jpg" class="img-responsive" alt="Travel">
+            <figcaption>Lorem ipsum dolar sit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
+        </figure>
+    </ul>
+</li>
+<!-- top 2 -->
+<li><a href="#">Real Estate</a>
+    <ul class="dropdown-menu">
+        <li><a href="#">Design &amp; Construction Info</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Architecture &amp; Engineering</a></li>
+                <li><a href="#">Design</a></li>
+                <li><a href="#">3D-4D Building Information Modeling</a></li>
+                <li><a href="#">CAD Standards</a></li>
+                <li><a href="#">Facilities Standards</a></li>
+                <li><a href="#">More Design &amp; Construction Info</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Environmental Programs</a></li>
+        <li><a href="#">Facilities &amp; Space Management</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Facilities Management Services</a></li>
+                <li><a href="#">Historic Preservation</a></li>
+                <li><a href="#">Leasing Policy and Procedures</a></li>
+                <li><a href="#">Real Property Disposal</a></li>
+                <li><a href="#">Reimbursable Services (RWAs)</a></li>
+                <li><a href="#">Rental Policy &amp; Procedures</a></li>
+                <li><a href="#">Telework</a></li>
+                <li><a href="#">Workplace Innovation</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Expolore Federal Buildings</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Historic Buildings</a></li>
+                <li><a href="#">Inventory of Owned and Leased Properties</a></li>
+                <li><a href="#">Open to the Public</a></li>
+                <li><a href="#">Smart Buildings</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Ink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <figure class="menu-image">
+            <img src="images/menu-realEstate.jpg" class="img-responsive" alt="Real Estate">
+            <figcaption>Lorem ipsum dolar sit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
+        </figure>
+    </ul>
+</li>
+<!-- top 3 -->
+<li><a href="shop.php">Shopper</a>
+    <ul class="dropdown-menu">
+        <li><a href="#">Mink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Snake</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Mink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">WINK</a></li>
+                <li><a href="#">WINK</a></li>
+                <li><a href="#">WINK</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Mink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Stink</a></li>
+                <li><a href="#">Stink</a></li>
+                <li><a href="#">Stink</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Mink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Pink</a></li>
+                <li><a href="#">Pink</a></li>
+                <li><a href="#">Pink</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Mink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <figure class="menu-image">
+            <img src="images/menu-shopper.jpeg" class="img-responsive" alt="Shoppers">
+            <figcaption>Lorem ipsum dolar sit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
+        </figure>
+    </ul>
+</li>
+<!-- top 4 -->
+<li><a href="#">Supplier</a>
+    <ul class="dropdown-menu">
+        <li><a href="#">Giraffe</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Dink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">WINK</a></li>
+                <li><a href="#">WINK</a></li>
+                <li><a href="#">WINK</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Dink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Stink</a></li>
+                <li><a href="#">Stink</a></li>
+                <li><a href="#">Stink</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Dink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Pink</a></li>
+                <li><a href="#">Pink</a></li>
+                <li><a href="#">Pink</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Dink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <figure class="menu-image">
+            <img src="images/menu-shopper.jpeg" class="img-responsive" alt="Shoppers">
+            <figcaption>Lorem ipsum dolar sit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
+        </figure>
+    </ul>
+</li>
+<!-- top 5 -->
+<li><a href="#">Technology</a>
+    <ul class="dropdown-menu">
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">WINK</a></li>
+                <li><a href="#">WINK</a></li>
+                <li><a href="#">WINK</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Stink</a></li>
+                <li><a href="#">Stink</a></li>
+                <li><a href="#">Stink</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Pink</a></li>
+                <li><a href="#">Pink</a></li>
+                <li><a href="#">Pink</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <figure class="menu-image">
+            <img src="images/menu-shopper.jpeg" class="img-responsive" alt="Shoppers">
+            <figcaption>Lorem ipsum dolar sit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
+        </figure>
+    </ul>
+</li>
+<!-- top 6 -->
+<li><a href="#">About Us</a>
+    <ul class="dropdown-menu">
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">WINK</a></li>
+                <li><a href="#">WINK</a></li>
+                <li><a href="#">WINK</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Stink</a></li>
+                <li><a href="#">Stink</a></li>
+                <li><a href="#">Stink</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Pink</a></li>
+                <li><a href="#">Pink</a></li>
+                <li><a href="#">Pink</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Sink</a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </li>
+        <figure class="menu-image">
+            <img src="images/menu-shopper.jpeg" class="img-responsive" alt="Shoppers">
+            <figcaption>Lorem ipsum dolar sit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
+        </figure>
+    </ul>
+</li>
+<li><a id="main-nav-search-button" href="#"><i class="glyphicon glyphicon-search"></i></a></li>
+</ul>
