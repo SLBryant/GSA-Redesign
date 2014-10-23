c089ea44af50b712b74bc24b1101dbfa55ca91da
Adam Pflantzer <apflantzer@gmail.com>
2014-10-23 10:22:26 -0400

    Box shadows on overview page buckets

--- partials/sub-section-travel.php
+++ partials/sub-section-travel.php
@@ -3,20 +3,25 @@
     <!-- Nav tabs -->
     <nav class="col-sm-3 tab-navigation">
         <ul class="nav" role="tablist">
-            <li class="active"><a href="#searchAdvantage" role="tab" data-toggle="tab">GSAAdvantage!</a></li>
-            <li><a href="#perDiemCalculator" role="tab" data-toggle="tab">Per Diem Calculator</a></li>
-            <li><a href="#lorem" role="tab" data-toggle="tab">Lorem Ipsum</a></li>
+            <li class="active"><a href="#perDiemCalculator" role="tab" data-toggle="tab">Per Diem Calculator</a></li>
+            <li><a href="#mie-breakdown" role="tab" data-toggle="tab">M&IE Breakdown</a></li>
+            <li><a href="#cityPairs" role="tab" data-toggle="tab">City Pairs Search</a></li>
+            <li><a href="#pov" role="tab" data-toggle="tab">POV Reimbursement Rates</a></li>
         </ul>
     </nav>
     <div class="col-sm-9 tab-content">
-        <div class="tab-pane active" id="searchAdvantage">
-            <form class="form-inline"><div class="form-group"><label class="sr-only" for="form-control">Search GSA Advantage</label> <input class="form-control col-sm-12" type="text" placeholder="Search GSA Advantage" /> <button id="searchAdvantageSubmit" class="btn gsa-btn" type="submit"><span class="glyphicon glyphicon-search"> </span></button>The online marketplace for Federal Procurement. Now also available to State and Local Employees.</div></form>
-        </div>
-        <div class="tab-pane" id="perDiemCalculator">
+        <div class="tab-pane active" id="perDiemCalculator">
             <form class="form-horizontal row"><div class="col-sm-6 content-column"><div class="form-group"><label class="sr-only" for="form-control">City</label> <input class="form-control" type="text" placeholder="City" /></div><div class="form-group"><label class="sr-only" for="form-control">Select a State</label><select class="form-control"><option>State</option><option>State</option><option>State</option><option>State</option><option>State</option></select></div><div class="form-group"><label class="sr-only" for="form-control">Zip</label> <input class="form-control" type="text" placeholder="Zip" /></div></div><div class="col-sm-3 content-column"><div class="form-group"><label class="sr-only" for="form-control">Start Date</label> <input class="form-control" type="text" placeholder="Start Date" /></div><div class="form-group"><label class="sr-only" for="form-control">End Date</label> <input class="form-control" type="text" placeholder="End Date" /></div><p><button class="btn gsa-btn btn-block" type="submit">Calculate</button></p></div><div class="col-sm-3 content-column"><button class="btn gsa-btn btn-block">Today's rate for my location</button></div></form>
         </div>
-        <div class="tab-pane" id="lorem">
-            Lorem ipsum dolar sit.
+        <div class="tab-pane" id="mie-breakdown">
+            Lorem
+        </div>
+        <div class="tab-pane" id="cityPairs">
+            <form class="form-inline"><div class="form-group"><label class="sr-only" for="form-control">Search City Pairs</label> <input class="form-control col-sm-12" type="text" placeholder="Search City Pairs" /> <button
+                            id="searchAdvantageSubmit" class="btn gsa-btn" type="submit"><span class="glyphicon glyphicon-search"> </span></button>The online marketplace for Federal Procurement. Now also available to State and Local Employees.</div></form>
+        </div>
+        <div class="tab-pane" id="pov">
+            Lorem
         </div>
     </div>
 </section>
